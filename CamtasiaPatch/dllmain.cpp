5e5ab251c30231d4bf0aa5e490a8fa1d45a99be0
lnx00 <34384416+lnx00@users.noreply.github.com>
2024-06-17 01:09:08 +0200

    Implemented shader patch

--- CamtasiaPatch/dllmain.cpp
+++ CamtasiaPatch/dllmain.cpp
@@ -13,16 +13,75 @@
 #include "ios"
 #include "fstream"
 
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+using TGetterFunction = bool();
+using TSetterFunction = void(bool bValue);
+
+bool PatchFunction(void* targetFunction, const BYTE* customBytes, SIZE_T customSize)
 {
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
-    }
-    return TRUE;
+	DWORD flOldProtect;
+
+	// Change memory protection to allow writing
+	if (VirtualProtect(targetFunction, customSize, PAGE_EXECUTE_READWRITE, &flOldProtect))
+	{
+		// Apply the custom byte sequence
+		std::memcpy(targetFunction, customBytes, customSize);
+
+		// Restore the original memory protection
+		return VirtualProtect(targetFunction, customSize, flOldProtect, &flOldProtect);
+	}
+
+	MessageBox(nullptr, TEXT("Failed to patch function!"), TEXT("Error"), 0);
+	return false;
 }
 
+DWORD WINAPI MainThread(LPVOID lpParam)
+{
+	// Wait for the target module
+	HMODULE target_module = nullptr;
+	while (!target_module)
+	{
+		Sleep(100);
+		target_module = GetModuleHandleA("CSMediaLib.dll");
+	}
+
+	// Find the target function
+	auto* target_function = reinterpret_cast<TGetterFunction*>(GetProcAddress(target_module, "?isSkipShaderSourceSignatureVerificationOn@FeatureSwitchSettings@CSMediaLib@@SA_NXZ"));
+	if (!target_function)
+	{
+		MessageBoxA(nullptr, "Target function not found!", "Error", MB_ICONERROR | MB_OK);
+		ExitThread(EXIT_SUCCESS);
+	}
+
+	// Apply the patch
+	const BYTE patch_bytes[] = {
+		0xB8, 0x01, 0x00, 0x00, 0x00, // mov eax, 1
+		0xC3 // retn
+	};
+	if (!PatchFunction(target_function, patch_bytes, sizeof(patch_bytes)))
+	{
+		MessageBoxA(nullptr, "Patch failed!", "Error", MB_ICONERROR | MB_OK);
+	}
+
+	ExitThread(EXIT_SUCCESS);
+}
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
+{
+	switch (fdwReason)
+	{
+		case DLL_PROCESS_ATTACH:
+		{
+			if (const HANDLE thread = CreateThread(nullptr, 0, MainThread, hinstDLL, 0, nullptr))
+			{
+				CloseHandle(thread);
+			}
+			break;
+		}
+
+		case DLL_THREAD_ATTACH:
+		case DLL_THREAD_DETACH:
+		case DLL_PROCESS_DETACH:
+			break;
+	}
+	return TRUE;
+}
