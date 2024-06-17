8d768f80c0d6bfd4720f91e18ec5dbece22a8836
lnx00 <34384416+lnx00@users.noreply.github.com>
2024-06-17 22:14:01 +0200

    Improved dot grid and added cloud shader

--- Shaders/dot_grid/dot_grid.glsl
+++ Shaders/dot_grid/dot_grid.glsl
@@ -1,17 +1,21 @@
 uniform float _XSpeed;
 uniform float _YSpeed;
 
+float smooth_circle_using_length(vec2 position, float radius) {
+    vec2 d = position - vec2(0.5);
+    return 1.0 - smoothstep(0.0, radius, length(d));
+}
+
 void main(void) {
     vec2 uv = _TexCoord * (_ImageSize / _ImageSize.y);
 
     float gridSize = 15.0;
     vec2 dotPos = fract((uv * gridSize) - vec2(_Time * _XSpeed, _Time * _YSpeed));
 
-    float dist = length(dotPos - 0.5);
-    float dot = smoothstep(0.02, 0.03, 0.05 - dist);
+    float dot = smooth_circle_using_length(dotPos, 0.1);
 
     vec3 bgColor = vec3(0.1, 0.1, 0.1);
-    vec3 dotColor = vec3(1.0, 1.0, 1.0);
+    vec3 dotColor = vec3(0.2, 0.2, 0.2);
 
     vec3 color = mix(bgColor, dotColor, dot);
     _FragColor = vec4(color, 1.0);
